4cc1fd5077411407ec8e4d1a166bb11abbd090af
forceyash <forceyash@yahoo.com>
2019-02-22 18:31:20 +0530

    Signed-off-by: forceyash <forceyash@yahoo.com>

--- IPL/Most-runs-in-IPL.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("deliveries.csv")
-print(df.groupby("batsman")["batsman_runs"].agg("sum").nlargest(10))
--- IPL/most-wickets.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_csv("C:\\Users\\Yash\\AppData\\Local\\Programs\\Python\\Python36-32\\Machine Learning\\IPL\\deliveries.csv")
-df = df[(df["is_super_over"] != 1)]
-df = df[df.player_dismissed.notnull()]
-df = df[(df["dismissal_kind"] != "run out") & (df["dismissal_kind"] != "obstructing the field") & (df["dismissal_kind"] != "retired hurt")]
-df["bowler"].value_counts()
